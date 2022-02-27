3f52a81bc3d2d4b7068777b65e5f23bba2dae410
ALI ZIA KHAN <azk443.443@gmail.com>
2022-02-27 20:25:14 +0500

    2

---
+++ DataTypes.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.1;
+
+
+contract Datatypes{
+
+
+//datatype visibility value
+bool public boolean =true;
+int public myInt = 1;
+uint public myUint = 1;
+uint256 public myUint256 = 9999;
+uint8 public myUint8 = 8;
+
+}
